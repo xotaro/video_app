73bb48c65f9d36ae6a48866ff63697aa58a61832
Mohamed98Ayman <mohamedayman9821@gmail.com>
2021-07-03 15:35:02 +0200

    login frontend

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:video_app/view/login_page.dart';
+import 'view/login_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
